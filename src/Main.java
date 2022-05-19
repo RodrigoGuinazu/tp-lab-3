ff9b7e0675aef4d44d36df8a4ae9118ba1d0319f
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2022-05-19 19:33:47 -0300

    prueba rodri commit intellij al mismo tiempo

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world!");
         System.out.println("intento pull mati");
         System.out.println("intento push inteli");
         System.out.println("push rodri intellij");
         System.out.println("mismo tiempo");
+        System.out.println("holahola");
     }
 }
