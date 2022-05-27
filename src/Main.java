94d08d53958bebf5b1ca98c0e8a415d943be832c
MatiasMantobani <matiasmantobani1@gmail.com>
2022-05-27 18:17:33 -0300

    prueba al mismo tiempo 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("TP LAB");
-        System.out.println("TP LAB 2");
+        System.out.println("PRUEBA 1");
     }
 }
