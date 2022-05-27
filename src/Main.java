88ceaaaf0070a4530da5b9b718e3d07482a27168
SantiTorrecillas <santiagotorrecillas99@gmail.com>
2022-05-27 18:03:46 -0300

    prueba push desde intelli

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("intento pull mati");
-        System.out.println("intento push inteli");
-        System.out.println("push rodri intellij");
-        System.out.println("mismo tiempo");
-        System.out.println("mismo tiempo2");
-        System.out.println("holahola");
+        System.out.println("TP LAB 3");
     }
 }
