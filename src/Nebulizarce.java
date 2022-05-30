b75faddbfb9f3e5686b95e4bd436e0185badcfc3
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2022-05-30 01:39:58 -0300

    Subiendo la clase Nebulizarce

---
+++ src/Nebulizarce.java
@@ -0,0 +1,14 @@
+public class Nebulizarce extends Accion{
+    // Atributos
+
+    // Constructores
+    public Nebulizarce(int duracion, int cadaCuanto, int vecesPorDia) {
+        super(duracion, cadaCuanto, vecesPorDia);
+    }
+
+    // Metodos
+    @Override
+    public void accionar() {
+
+    }
+}
