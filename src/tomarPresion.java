365cac302d464fede63b7c25a4a827675bab40d2
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2022-05-30 01:41:29 -0300

    Subiendo la clase tomarPresion

---
+++ src/tomarPresion.java
@@ -0,0 +1,15 @@
+public class tomarPresion extends Accion{
+    // Atributos
+
+    // Constructores
+    public tomarPresion(int duracion, int cadaCuanto, int vecesPorDia) {
+        super(duracion, cadaCuanto, vecesPorDia);
+    }
+
+
+    // Metodos
+    @Override
+    public void accionar() {
+
+    }
+}
