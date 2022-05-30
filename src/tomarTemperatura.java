3efc6ba5c1316bca290054b037023c33cbe40cdf
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2022-05-30 01:41:53 -0300

    Subiendo la clase tomarTemperatura

---
+++ src/tomarTemperatura.java
@@ -0,0 +1,14 @@
+public class tomarTemperatura extends Accion{
+    // Atributos
+
+    // Constructores
+    public tomarTemperatura(int duracion, int cadaCuanto, int vecesPorDia) {
+        super(duracion, cadaCuanto, vecesPorDia);
+    }
+
+    // Metodos
+    @Override
+    public void accionar() {
+
+    }
+}
