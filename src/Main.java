27411bf31b13fc48f36529b8748e322278736199
ak4ci <matiasmantobani1@gmail.com>
2022-06-08 22:47:25 -0300

    mejoras sintaxis menues + persistencia enfermedades

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,11 @@ public class Main {
 //        Sistema sistema = new Sistema();
 //        sistema.menu();
 
+
         // pacientes
         ArrayList<Paciente> pacientes = new ArrayList<>();
-
-        Paciente paciente1 = new Paciente("Juan", "Ros", "11247824", "paciente@gmail.com", "Casa");
-        pacientes.add(paciente1);   //sin medico asignado
-
-        Paciente paciente2 = new Paciente("Elena", "Vargas", "40347821", "paciente@hotmail.com", "Coco", 14);
-        pacientes.add(paciente2);   //con medico asignado
-//        System.out.println(pacientesMain);
+        pacientes.add(new Paciente("Juan", "Ros", "11247824", "paciente@gmail.com", "Casa"));//sin medico asignado
+        pacientes.add(new Paciente("Elena", "Vargas", "40347821", "paciente@hotmail.com", "Coco", 14));//con medico asignado
         Persistencia.serializacion(pacientes, "pacientes.json");
 
 
@@ -24,35 +20,29 @@ public class Main {
         pacientesMedico.add(1);
         pacientesMedico.add(3);
         pacientesMedico.add(7);
-
-        Medico medico1 = new Medico("Sandra", "Gonzales", "44963081", "medico@outlook.com", "Cielo");
-        medicos.add(medico1);   //sin lista de pacientes
-
-        Medico medico2 = new Medico("Cintia", "Laiz", "56912074", "medico@gmail.com", "Cosa", pacientesMedico);
-        medicos.add(medico2);   //con lista de pacientes
-
+        medicos.add(new Medico("Sandra", "Gonzales", "44963081", "medico@outlook.com", "Cielo"));   //sin lista de pacientes
+        medicos.add(new Medico("Cintia", "Laiz", "56912074", "medico@gmail.com", "Cosa", pacientesMedico));   //con lista de pacientes
         Persistencia.serializacion(medicos, "medicos.json");
 
 
         //acciones
         ArrayList<Accion> acciones = new ArrayList<>();
-
-
         acciones.add(new AccionBooleana(1, 4, "Pastilla", "Tomar Pastilla"));
         acciones.add(new AccionBooleana(1, 2, "Presion", "Tomar Presion"));
-
         acciones.add(new AccionDouble(1,3,"Temperatura","Tomar Temperatura"));
         acciones.add(new AccionDouble(2,1,"Densiometria","Tomar Densidad"));
-
         Persistencia.serializacion(acciones, "acciones.json");
 
 
 
-//        //enfermedades
-//        ArrayList<Enfermedad> enfermedades = new ArrayList<>();
-//     Enfermedad enfermedad1 = new Enfermedad("Fiebre");
-//
-
+//        //Enfermedades
+        ArrayList<Enfermedad> enfermedades = new ArrayList<>();
+        enfermedades.add(new Enfermedad("Estres"));
+        enfermedades.add(new Enfermedad("Gripe"));
+        enfermedades.add(new Enfermedad("Alergia"));
+        enfermedades.add(new Enfermedad("Amiloidosis"));
+        enfermedades.add(new Enfermedad("Encondroma"));
+        Persistencia.serializacion(enfermedades,"enfermedades.json");
 
 
 
